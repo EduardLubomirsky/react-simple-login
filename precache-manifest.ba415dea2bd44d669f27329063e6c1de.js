1204b8b020e4efa36cc0a3f0f0c0c89933a0166c
Eduard Lubomirsky <eduard.lubomirsky.gmail.com>
2019-12-10 16:11:28 +0200

    Updates

---
+++ precache-manifest.ba415dea2bd44d669f27329063e6c1de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13fd8a53038b5077a3fcc5dd1a12116a",
+    "url": "/react-simple-login/index.html"
+  },
+  {
+    "revision": "ef5e1eda6db40ac829b5",
+    "url": "/react-simple-login/static/css/main.8dae5b8c.chunk.css"
+  },
+  {
+    "revision": "abf9991a34563e3668c5",
+    "url": "/react-simple-login/static/js/2.525de572.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/react-simple-login/static/js/2.525de572.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef5e1eda6db40ac829b5",
+    "url": "/react-simple-login/static/js/main.255f783f.chunk.js"
+  },
+  {
+    "revision": "c386d1c5f11caaeff97d",
+    "url": "/react-simple-login/static/js/runtime-main.c7f99697.js"
+  },
+  {
+    "revision": "862d26fae48a249708c586d9183f58fa",
+    "url": "/react-simple-login/static/media/logo.862d26fa.png"
+  }
+]);
